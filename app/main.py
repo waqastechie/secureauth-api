ac56838f4ef845443fbfb73cd7b9cc25e574799a
waqastehcie <waqastechie@gmail.com>
2026-05-30 16:58:43 +0100

    Add PostgreSQL database connection and User model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
+from app.database import engine, Base
+from app import models
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="SecureAuth API")
+
 
 @app.get("/")
 def root():
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(150), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
